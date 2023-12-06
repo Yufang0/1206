84e60bf196727d220c2ae6181adb2d21a29cda81
Yufang <411422259@m365.fju.edu.tw>
2023-12-06 16:37:35 +0800

    擷取等候掛號人數

--- 1206_1.R
+++ 1206_1.R
@@ -8,11 +8,34 @@ head(txt, 15)
 #把被分段的字串，和成一個長字串
 txt_new = paste(txt, sep = "", collapse = " ")
 
-
+#以正則表達式，擷取網頁標題
 TITLE.pos = gregexpr("<title>.*</title>", txt_new)
+#找到頭尾
 start.TITLE.pos = TITLE.pos[[1]][1]
 end.TITLE.pos = start.TITLE.pos + attr(TITLE.pos[[1]], "match.length")[1] - 1
-
+#擷取標題
 TITLE.word = substr(txt_new, start.TITLE.pos, end.TITLE.pos)
-
 TITLE.word
+
+# 將標題的 <title> </title> 拿掉
+TITLE.word = gsub("<title>", "", TITLE.word)
+TITLE.word = gsub("</title>", "", TITLE.word)
+TITLE.word
+
+
+#擷取等候掛號人數
+#因為tr結尾的有非常多，所以必須分開擷取
+start.pos = gregexpr("<tr>", txt_new)
+end.pos = gregexpr("</tr>", txt_new)
+#第一個就是等候掛號人數
+i = 1
+sub.start.pos = start.pos[[1]][i]
+sub.end.pos = end.pos[[1]][i] + attr(end.pos[[1]], "match.length")[i] - 1
+sub_txt = substr(txt_new, sub.start.pos, sub.end.pos)
+sub_txt
+#清理成只剩下 ?人
+sub_txt = gsub('等候掛號人數：', '', sub_txt)
+sub_txt = gsub('</?tr>', '', sub_txt)
+sub_txt = gsub('</?td>', '', sub_txt)
+sub_txt = gsub(' ', '', sub_txt)
+sub_txt
