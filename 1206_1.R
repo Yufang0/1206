9bcb6c9df75aa5c0df9166c6562fcdf71f984211
Yufang <411422259@m365.fju.edu.tw>
2023-12-06 16:41:27 +0800

    自訂函數抓臺大醫院的急診即時訊息

--- 1206_1.R
+++ 1206_1.R
@@ -8,6 +8,7 @@ head(txt, 15)
 #把被分段的字串，和成一個長字串
 txt_new = paste(txt, sep = "", collapse = " ")
 
+
 #以正則表達式，擷取網頁標題
 TITLE.pos = gregexpr("<title>.*</title>", txt_new)
 #找到頭尾
@@ -39,3 +40,38 @@ sub_txt = gsub('</?tr>', '', sub_txt)
 sub_txt = gsub('</?td>', '', sub_txt)
 sub_txt = gsub(' ', '', sub_txt)
 sub_txt
+
+
+#P01寫出一個函數讓我能隨時知道臺大醫院的急診即時訊息
+NTU_info = function () {
+  
+  result = data.frame(item = c('等候掛號人數', '等候看診人數', '等候住院人數', '等候ICU人數', '等候推床人數'),
+                      info = NA,
+                      stringsAsFactors = FALSE)
+  
+  URL = "https://reg.ntuh.gov.tw/EmgInfoBoard/NTUHEmgInfo.aspx"
+  
+  txt = scan(URL, what = "character", encoding = "UTF-8", quiet = TRUE)
+  txt_new = paste(txt, sep = "", collapse = " ")
+  
+  start.pos = gregexpr("<tr>", txt_new)
+  end.pos = gregexpr("</tr>", txt_new)
+  
+  for (i in 1:5) {
+    
+    sub.start.pos = start.pos[[1]][i]
+    sub.end.pos = end.pos[[1]][i] + attr(end.pos[[1]], "match.length")[i] - 1
+    
+    sub_txt = substr(txt_new, sub.start.pos, sub.end.pos)
+    sub_txt = gsub('等.*：', '', sub_txt)
+    sub_txt = gsub('</?tr>', '', sub_txt)
+    sub_txt = gsub('</?td>', '', sub_txt)
+    result[i,'info'] = gsub(' ', '', sub_txt)
+    
+  }
+  
+  result
+  
+}
+
+NTU_info()
